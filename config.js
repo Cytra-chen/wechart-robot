09bec9bfd55e30cba64e2bc341f25d56647ddcfb
cytra <222151155@qq.com>
2023-03-23 14:22:42 +0800

    feat: 加入config

---
+++ config.js
@@ -0,0 +1,24 @@
+//微信机器人登录需要的token
+//购买地址： http://pad-local.com/ ，注册送一个7天的
+export const PAD_LOCAL_TOKEN = ''
+
+//openAI的token
+//登陆后可创建：https://platform.openai.com/account/api-keys
+//organization在组织设置里获取：https://platform.openai.com/account/org-settings
+export const CHATGPT_TOKEN = {
+    apiKey: '',
+    organization: ''
+}
+
+//百度云API的公私钥，用于文字识别OCR
+//免费申请：https://console.bce.baidu.com/ai/#/ai/ocr/app/list
+export const BAIDU_TOKEN = {
+    API_Key: '',
+    Secret_Key: ''
+}
+
+//OA账号
+export const HDEC_TOKEN = {
+    username: '',
+    password: ''
+}
